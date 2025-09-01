9a5bdf91ac32a262e0556806f6ced9606da1698b
Valerio Barbera <valerio@inspector.dev>
2025-09-01 23:00:01 +0200

    Update unix.php

--- unix.php
+++ unix.php
@@ -2,7 +2,6 @@
 
 use App\DeepResearchAgent;
 use App\Events\ProgressEvent;
-use Gbhorwood\Macrame\Macrame;
 
 
 include __DIR__ . '/vendor/autoload.php';
@@ -10,39 +9,22 @@ include __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// instantiate a Macrame object with the script name
-$macrame = new Macrame("NeuronAI Workflow Demo");
-
-// only execute if run from the command line
-if($macrame->running()) {
-    // confirm host is good. die on failure.
-    $macrame->preflight();
-}
-
-// Run the workflow
-$run = function (string $input, Macrame $macrame): void {
-    $workflow = new DeepResearchAgent($input, 2); // Limit to 1 for testing
-
-    $handler = $workflow->start();
-
-    foreach ($handler->streamEvents() as $event) {
-        if ($event instanceof ProgressEvent) {
-            $macrame->text($event->message)->write();
-        }
-    }
-
-    $macrame->text($handler->getResult()->get('report'))->write(true);
-};
-
 // Interactive console
-$input = $macrame->input()->readline("Describe the topic: ");
+echo 'Describe the topic: ';
+$input = \rtrim(\fgets(STDIN));
 
 if (empty($input)) {
-    $macrame->exit();
+    exit(0);
 }
 
-$macrame->spinner('dots 3')
-    ->colour('green')
-    ->run($run, [$input, $macrame]);
+$workflow = new DeepResearchAgent($input, 1);
 
-$macrame->exit();
+$handler = $workflow->start();
+
+foreach ($handler->streamEvents() as $event) {
+    if ($event instanceof ProgressEvent) {
+        echo $event->message;
+    }
+}
+
+echo "\n".$handler->getResult()->get('report')."\n";
